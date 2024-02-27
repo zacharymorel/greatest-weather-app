998c971af0113e2f7bf3c77072c827aa8ecd541b
zacharymorel <zacharyjmorel@gmail.com>
2024-02-27 16:22:37 -0500

    Styling and versoin bump

--- components/common/Loader.tsx
+++ components/common/Loader.tsx
@@ -1,9 +1,9 @@
 function Loader(): JSX.Element {
   return (
     <>
-      <span className="ball -right-1 w-1 h-1 rounded-half absolute" />
-      <span className="ball -right-2 w-1 h-1 rounded-half absolute" />
-      <span className="ball -right-3 w-1 h-1 rounded-half absolute" />
+      <span className="ball -right-1 w-2 h-2 rounded-half absolute" />
+      <span className="ball -right-3 w-2 h-2 rounded-half absolute" />
+      <span className="ball -right-5 w-2 h-2 rounded-half absolute" />
     </>
   );
 }
